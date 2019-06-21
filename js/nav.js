c9866a33f3338e9a6ddd273b78dd84ac0cbe6cb5
Christopher Ksiazak <cksiazak@gmail.com>
2019-06-20 20:03:47 -0600

    added a javascript file to change nav color on scroll

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+window.onscroll = () => {
+    const nav = document.querySelector('.header-container');
+    if(this.scrollY >= 10){
+        nav.classList.add('nav-fixed-change')
+    } else {
+        nav.classList.remove('nav-fixed-change');
+    }
+  };
