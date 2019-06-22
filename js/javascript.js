26ff27c19ef3691fbd42b91c678017a5fb34464d
Christopher Ksiazak <cksiazak@gmail.com>
2019-06-21 20:29:44 -0600

    completed hamburger menu animation

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,25 @@
 document.addEventListener("scroll", () => {
     const nav = document.querySelector('.header-container');
+    const hamburger = document.querySelector('.hamburger-menu');
     if (this.scrollY >= 10) {
+        hamburger.classList.remove('hamburger-open-timing');
         nav.classList.add('nav-fixed-change')
+        hamburger.classList.add('hamburger-menu-scroll');
     } else {
         nav.classList.remove('nav-fixed-change');
+        hamburger.classList.remove('hamburger-menu-scroll');
+        hamburger.classList.add('hamburger-open-timing');
     }
 });
+
+const hamburgerIcon = document.querySelector('.hamburger-menu');
+
+hamburgerIcon.addEventListener('click', () => {
+    const hamburger = document.querySelector('.hamburger-menu');
+    if (hamburger.classList.contains('hamburger-menu-click')) {
+        hamburger.classList.remove('hamburger-menu-click');
+    } else {
+        hamburger.classList.remove('hamburger-menu-click');
+        hamburger.classList.add('hamburger-menu-click')
+    }
+})
