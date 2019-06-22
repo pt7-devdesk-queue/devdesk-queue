8187214a2ad4b094fa1608b1ba6d8263781d25fc
Christopher Ksiazak <cksiazak@gmail.com>
2019-06-21 22:21:17 -0600

    changed images and modified styling for teams section

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,30 @@
+//Hamburger Menu transitions and functions
 document.addEventListener("scroll", () => {
     const nav = document.querySelector('.header-container');
     const hamburger = document.querySelector('.hamburger-menu');
-    if (this.scrollY >= 10) {
+    if (this.scrollY >= 150) {
         hamburger.classList.remove('hamburger-open-timing');
-        nav.classList.add('nav-fixed-change')
+        nav.classList.add('nav-fixed-change');
         hamburger.classList.add('hamburger-menu-scroll');
+        nav.classList.remove('nav-hamburger-clicked');
+        hamburger.classList.remove('hamburger-menu-click');
     } else {
         nav.classList.remove('nav-fixed-change');
         hamburger.classList.remove('hamburger-menu-scroll');
         hamburger.classList.add('hamburger-open-timing');
     }
 });
-
 const hamburgerIcon = document.querySelector('.hamburger-menu');
-
 hamburgerIcon.addEventListener('click', () => {
     const hamburger = document.querySelector('.hamburger-menu');
-    if (hamburger.classList.contains('hamburger-menu-click')) {
+    const nav = document.querySelector('.header-container');
+
+    if (!hamburger.classList.contains('hamburger-menu-click')) {
+        hamburger.classList.add('hamburger-menu-click');
+        nav.classList.add('nav-hamburger-clicked');
+    } else if (hamburger.classList.contains('hamburger-menu-click')) {
         hamburger.classList.remove('hamburger-menu-click');
-    } else {
-        hamburger.classList.remove('hamburger-menu-click');
-        hamburger.classList.add('hamburger-menu-click')
+        nav.classList.remove('nav-hamburger-clicked');
     }
-})
+});
+
