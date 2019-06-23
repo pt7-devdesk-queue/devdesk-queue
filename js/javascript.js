733f46dea7b809aa198e2658694c1c852048e70a
Christopher Ksiazak <cksiazak@gmail.com>
2019-06-22 19:52:52 -0600

    added js to remove extended nav when nav drops below 600px

--- js/javascript.js
+++ js/javascript.js
@@ -4,7 +4,7 @@ document.addEventListener("scroll", () => {
     const hamburger = document.querySelector('.hamburger-menu');
     const devDeskLogo = document.querySelector('.header-container h1');
     const mq = window.matchMedia("(max-width: 600px)");
-    if (!mq.matches){
+    if (!mq.matches) {
         if (this.scrollY >= 150) {
             hamburger.classList.remove('hamburger-open-timing');
             nav.classList.add('nav-fixed-change');
@@ -18,8 +18,17 @@ document.addEventListener("scroll", () => {
             hamburger.classList.add('hamburger-open-timing');
             devDeskLogo.classList.add('devDesk-Shadow');
         }
+    } else if (mq.matches) {
+        nav.classList.remove('nav-hamburger-clicked');
     }
 });
+window.addEventListener('resize', () => {
+    const nav = document.querySelector('.header-container');
+    if (this.innerWidth < 600) {
+        nav.classList.remove('nav-fixed-change');
+    }
+});
+
 const hamburgerIcon = document.querySelector('.hamburger-menu');
 hamburgerIcon.addEventListener('click', () => {
     const hamburger = document.querySelector('.hamburger-menu');
